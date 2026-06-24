48ed495c56ac808a01c71f2b44739f1b0e4c5541
Morpheus <vedanshagarwaldpr1234@gmail.com>
2026-06-24 22:41:18 +0530

    Time: 61 ms (5.92%), Space: 48.7 MB (96.49%) - LeetHub

---
+++ 0992-subarrays-with-k-different-integers/0992-subarrays-with-k-different-integers.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int subarraysWithKDistinct(int[] nums, int k) {
+        return atmostk(nums, k) - atmostk(nums, k - 1);
+    }
+
+    public int atmostk(int[] nums, int K) {
+        Map<Integer, Integer> freq = new HashMap<>();
+        int left = 0, count = 0;
+        for (int right = 0; right < nums.length; right++) {
+            freq.put(nums[right], freq.getOrDefault(nums[right], 0) + 1);
+            if (freq.get(nums[right]) == 1) {
+                K--;
+            }
+            while (K < 0) {
+                freq.put(nums[left], freq.get(nums[left]) - 1);
+                if (freq.get(nums[left]) == 0) {
+                    K++;
+                }
+                left++;
+            }
+            count += (right - left + 1);
+        }
+        return count;
+    }
+}
