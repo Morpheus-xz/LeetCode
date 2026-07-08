6592f8a3228d73d77fd72830b202652b0481288d
Morpheus <vedanshagarwaldpr1234@gmail.com>
2026-07-08 15:50:51 +0530

    Time: 3 ms (94.45%), Space: 83.3 MB (30.41%) - LeetHub

---
+++ 2498-frog-jump-ii/2498-frog-jump-ii.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int maxJump(int[] stones) {
+        int n = stones.length;
+        int re = stones[1]-stones[0];
+        for(int i=2;i<n;i++){
+            re = Math.max(re,stones[i]-stones[i-2]);
+        }
+        return re;
+    }
+}
