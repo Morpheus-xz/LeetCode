f4cc498ad06097138d1918dbc28174636d054685
Morpheus <vedanshagarwaldpr1234@gmail.com>
2026-07-08 13:54:48 +0530

    Time: 0 ms (100%), Space: 42.2 MB (33.34%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,6 +1,5 @@
 // ----------MEMOIZATION APPROACH------------
 // class Solution {
-//     // memoization approach
 //     public int fib(int n) {
 //         int dp[] = new int[n+1];
 //         Arrays.fill(dp,-1);
@@ -14,15 +13,30 @@
 // }
 
 // ----------TABULATION APPROACH------------
+// class Solution{
+//     public int fib(int n){
+//         if(n<=1) return n;
+//         int dp[] = new int[n+1];
+//         dp[0]=0;
+//         dp[1]=1;
+//         for(int i=2;i<=n;i++){
+//             dp[i] = dp[i-1]+dp[i-2];
+//         }
+//         return dp[n];
+//     }
+// }
+
+// ----------TABULATION APPROACH O(1)------------
 class Solution{
     public int fib(int n){
         if(n<=1) return n;
-        int dp[] = new int[n+1];
-        dp[0]=0;
-        dp[1]=1;
+        int prev2 = 0;
+        int prev1 = 1;
         for(int i=2;i<=n;i++){
-            dp[i] = dp[i-1]+dp[i-2];
+            int curr = prev2+prev1;
+            prev2=prev1;
+            prev1=curr;
         }
-        return dp[n];
+        return prev1;
     }
 }
