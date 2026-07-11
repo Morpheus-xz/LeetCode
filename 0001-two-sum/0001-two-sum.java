5d322ac87e1e10b85a3df52e6f250632e455aeaa
Morpheus <vedanshagarwaldpr1234@gmail.com>
2026-07-11 22:32:08 +0530

    Time: 2 ms (99.32%), Space: 47.2 MB (39.73%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer,Integer> mp = new HashMap<>();
+        int n = nums.length;
+        for(int i=0;i<n;i++){
+            int c = target-nums[i];
+            if(mp.containsKey(c)){
+                return new int[]{mp.get(c),i};
+            }
+            mp.put(nums[i],i);
+        }
+        return new int[]{};
+    }
+}
