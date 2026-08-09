102cdcabdbe01e214fce53ce847e31c137c8d573
Morpheus <vedanshagarwaldpr1234@gmail.com>
2026-08-09 22:19:12 +0530

    Time: 2 ms (96.3%), Space: 45.7 MB (68.47%) - LeetHub

---
+++ 0039-combination-sum/0039-combination-sum.java
@@ -0,0 +1,21 @@
+class Solution {
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> ans = new ArrayList<>();
+        fc(0,candidates,target,ans,new ArrayList<>());
+        return ans;
+    }
+    public void fc(int index,int[] arr, int target,List<List<Integer>> ans,List<Integer>ds){
+        if(index==arr.length){
+            if(target==0){
+                ans.add(new ArrayList<>(ds));
+            }
+            return;
+        }
+        if(arr[index]<=target){
+            ds.add(arr[index]);
+            fc(index,arr,target-arr[index],ans,ds);
+            ds.remove(ds.size()-1);
+        }
+        fc(index+1,arr,target,ans,ds);
+    }
+}
